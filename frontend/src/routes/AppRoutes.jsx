a74672f2f60b68820c17d2b4f002350c1a211f00
Pranav Pawar <pranavpawar776@gmail.com>
2026-07-16 15:09:27 +0530

    created all customer pages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
 import Dashboard from "../Pages/dashboard/Dashboard";
 
+import AddCustomer from "../Pages/customer/add-customer";
+
+
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DashboardLayout />}>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/add-customer" element={<AddCustomer />} />
         </Route>
       </Routes>
     </BrowserRouter>
