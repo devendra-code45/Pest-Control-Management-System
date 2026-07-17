a8525405e936061198211fc2312bd9160eb1ca68
Devendra Chaudhari <devendrachaudhari3108@gmail.com>
2026-07-17 11:58:37 +0530

    fixed pages width

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,9 @@ import DashboardLayout from "../components/DashboardLayout/DashboardLayout";
 import Dashboard from "../Pages/dashboard/Dashboard";
 
 import AddCustomer from "../Pages/customer/add-customer";
+import EditCustomer from "../Pages/customer/edit-customer";
+import CustomerList from "../Pages/customer/customer-list";
+import CustomerDetails from "../Pages/customer/customer-details";
 
 import NewComplaint from "../Pages/complaint/newcomplaint";
 import ComplaintManagement from "../Pages/complaint/complaintmanagement";
@@ -19,9 +22,15 @@ function AppRoutes() {
       <Routes>
         <Route element={<DashboardLayout />}>
           <Route path="/" element={<Dashboard />} />
+
           <Route path="/add-customer" element={<AddCustomer />} />
+          <Route path="/edit-customer" element={<EditCustomer />} />
+          <Route path="/customer-details" element={<CustomerDetails />} />
+          <Route path="/customer-list" element={<CustomerList />} />
+
           <Route path="/new-complaint" element={<NewComplaint />} />
           <Route path="/complaint" element={<ComplaintManagement />} />
+
           <Route path="/technician-management" element={<TechnicianManagement />} />
           <Route path="/add-technician" element={<AddTechnician />} />
           <Route path="/assign-technician" element={<AssignTechnician />} />
